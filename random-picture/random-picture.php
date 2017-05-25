4e1cd5a1a29749fbc0605e6acf646feb44ba8863
Kevin Landolt <git@klandolt.ch>
2017-05-25 16:31:17 +0200

    Reorganisation Widget in Unterverzeichnis / Registrieren des Aktivierungs Hooks und prüfen ob das Plugin Photo Gallery aktiviert ist.

--- random-picture/random-picture.php
+++ random-picture/random-picture.php
@@ -7,6 +7,21 @@ Version: 1.0
 
 */
 setlocale(LC_ALL,'de_DE.UTF-8');
+define( 'RP_PLUGIN_FILE', __FILE__ );
+ 
+register_activation_hook( RP_PLUGIN_FILE, 'rp_activation' );
+ 
+function rp_activation(){
+	//Check Photo-Gallery Plugin exist
+	if ( !is_plugin_active( 'photo-gallery/photo-gallery.php' ) ) {
+		
+		deactivate_plugins( plugin_basename( __FILE__ ) );
+		wp_die( __( 'Please install and activate Photo-Gallery Plugin', 'Random-Picture' ), 'Plugin dependency check', array( 'back_link' => true ) );
 
-require_once('widget.php');
+} 
+	
+}
+
+
+require_once('widget/widget.php');
 ?>
